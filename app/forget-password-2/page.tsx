c368c69b16112943b05819b98931c32497f9a43e
roscoericch <sikirurazak1@gmail.com>
2024-01-23 20:06:35 +0100

    fixed minor bug

--- app/forget-password-2/page.tsx
+++ app/forget-password-2/page.tsx
@@ -2,9 +2,9 @@
 import PasswordResetV2 from "@/component/password-reset-v2/PasswordResetV2";
 import { useSearchParams } from "next/navigation";
 
-const page = () => {
+const Page = () => {
   const params = useSearchParams();
   return <PasswordResetV2 email={params.get("email") || ""} />;
 };
 
-export default page;
+export default Page;
