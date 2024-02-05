9a83d2ea66ddb71d27f75b5abe039fc6c0ff2fd0
roscoericch <sikirurazak1@gmail.com>
2024-02-05 19:52:28 +0100

    update email verify business logic

--- component/signupBusness/SignupBusness.tsx
+++ component/signupBusness/SignupBusness.tsx
@@ -48,7 +48,7 @@ const SignupBusness = () => {
           .then(() => {
             setFormData(initailState);
             replace("/verifyEmail");
-            localStorage.setItem(`verify-${profile?.email}`, "true");
+            localStorage.setItem(`verify-${profile?.email}`, "false");
           })
           .catch(() => {
             setFormData(initailState);
