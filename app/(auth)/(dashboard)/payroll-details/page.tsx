2af570daeb1c5088071ce73bb3bfb47e7b0ba331
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-02-28 11:33:59 +0100

    chore: overview and details | rename to 'jargon'

---
+++ app/(auth)/(dashboard)/payroll-details/page.tsx
@@ -0,0 +1,8 @@
+import Details from "@/component/dashboard-component/payroll/details/Details";
+import React from "react";
+
+const page = () => {
+  return <Details />;
+};
+
+export default page;
--- component/dashboard-component/payroll/overview/Overview.tsx
+++ component/dashboard-component/payroll/overview/Overview.tsx
@@ -1,42 +1,24 @@
 "use client";
-import AddIcon from "@/assets/icon/AddIcon";
-import { CustomButton as Button } from "@/lib/AntdComponents";
-import OverviewTable from "./OverviewTable";
+
+import React from "react";
 import { useRouter } from "next/navigation";
-import Link from "next/link";
 
 const Overview = () => {
   const { push } = useRouter();
   return (
-    <div className="max-w-[1640px] flex flex-col p-4  h-screen overflow-y-scroll space-y-8 bg-[#FAFAFA]">
-      <header className="flex justify-between items-start ">
-        <div className="flex flex-col items-start space-y-4 ">
-          <h2 className="text-[18px] text-[#061A14] font-medium">
-            Add employees and contractors
-          </h2>
-          <span className="bg-white rounded-[5px] px-[20px] py-[18px] flex flex-col gap-2">
-            <h6 className="text-[#181336] text-[16px] font-[700]">Add One</h6>
-            <span className="flex items-center gap-2">
-              <p className="text-[] text-[14px] font-[400]">
-                Add Single Employee or a Contractor in Second
-              </p>
-              <Button
-                className="!flex !m-auto !items-center !border-0"
-                icon={<AddIcon />}
-                onClick={() => push("/add-payroll")}
-              />
-            </span>
-          </span>
-        </div>
-        {/* <button
-          // onClick={() => setIsModalOpen(true)}
-          className="btn btn-md  bg-black hover:bg-black text-white text-sm normal-case"
-        >
-          + Run Payroll
-        </button> */}
-      </header>
-      <OverviewTable />
-    </div>
+    <section className="max-w-[1640px] flex flex-col p-4 h-screen overflow-y-scroll space-y-8 bg-[#FAFAFA]">
+      <span>
+        <h2 className="text-[32px] text-[#061A14] font-bold">Payroll</h2>
+        <p className="font-normal text-[14px] text-[#84818A]">
+          Showing your Account metrics for July 19, 2021 - July 25, 2021
+        </p>
+      </span>
+      <div>
+        <button type="button" onClick={() => push("/payroll-details")}>
+          View Payroll
+        </button>
+      </div>
+    </section>
   );
 };
 
