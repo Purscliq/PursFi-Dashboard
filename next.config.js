2a57e58fc84065ccbece4ffe6fe3e33d5efa9610
sikiru razak <sikiru@nubytes.us>
2024-03-21 12:45:19 +0100

    update icons

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["flagcdn.com", "pusrfi.nyc3.digitaloceanspaces.com"],
+    domains: [
+      "flagcdn.com",
+      "pusrfi.nyc3.digitaloceanspaces.com",
+      "res.cloudinary.com",
+    ],
   },
 };
 
